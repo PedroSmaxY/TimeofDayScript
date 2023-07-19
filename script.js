f9a799ea754d509612f1311a3317c9b924a6364e
PedroSmaxY <pedrohnovais@outlook.com>
2023-07-19 13:35:16 -0300

    formated

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function carregar(){
-    let msg = window.document.querySelector('div#msg')
-    let img = window.document.getElementById('imagem')
-    let data = new Date()
-    let horaAtual = data.getHours()
-    msg.innerHTML = `Agora são ${horaAtual} hora(s).`
-    if (horaAtual >= 6 && horaAtual < 12) {
-        img.src = "manha.png"
-        msg.innerHTML += '<p>Bom Dia!</p>'
-        window.document.body.style.background = '#da955d'
-    } else if (horaAtual >= 12 && horaAtual < 18) {
-        img.src = "tarde.png"
-        msg.innerHTML += '<p>Boa Tarde!</p>'
-        window.document.body.style.background = '#CA6F0D'
-    } else if (horaAtual >= 0 && horaAtual < 6) {
-        img.src = "noite.png"
-        msg.innerHTML += '<p>Boa Madrugada!</p>'
-        window.document.body.style.background = '#513859'
-    } else {
-        //Boa Noite!
-        img.src = "noite.png"
-        msg.innerHTML +='<p>Boa Noite!</p>'
-        window.document.body.style.background = '#513859'
-    } 
-} 
+function carregar() {
+  let msg = window.document.querySelector("div#msg");
+  let img = window.document.getElementById("imagem");
+  let data = new Date();
+  let horaAtual = data.getHours();
+  msg.innerHTML = `Agora são ${horaAtual} hora(s).`;
+  if (horaAtual >= 6 && horaAtual < 12) {
+    img.src = "manha.png";
+    msg.innerHTML += "<p>Bom Dia!</p>";
+    window.document.body.style.background = "#da955d";
+  } else if (horaAtual >= 12 && horaAtual < 18) {
+    img.src = "tarde.png";
+    msg.innerHTML += "<p>Boa Tarde!</p>";
+    window.document.body.style.background = "#CA6F0D";
+  } else if (horaAtual >= 0 && horaAtual < 6) {
+    img.src = "noite.png";
+    msg.innerHTML += "<p>Boa Madrugada!</p>";
+    window.document.body.style.background = "#513859";
+  } else {
+    //Boa Noite!
+    img.src = "noite.png";
+    msg.innerHTML += "<p>Boa Noite!</p>";
+    window.document.body.style.background = "#513859";
+  }
+}
