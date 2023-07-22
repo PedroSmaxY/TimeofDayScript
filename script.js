ac322c4418043ed844f587af61774fab666cac28
PedroSmaxY <pedrohnovais@outlook.com>
2023-07-21 22:37:46 -0300

    Translated the content from Portuguese to English.

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ function load() {
   let msg = window.document.querySelector("div#msg");
   let img = window.document.getElementById("imagem");
   let data = new Date();
-  let horaAtual = data.getHours();
-  msg.innerHTML = `Agora são ${horaAtual} hora(s).`;
-  if (horaAtual >= 6 && horaAtual < 12) {
-    img.src = "manha.png";
-    msg.innerHTML += "<p>Bom Dia!</p>";
+  let currentHour = data.getHours();
+  msg.innerHTML = `It's currently ${currentHour} hour(s).`;
+  if (currentHour >= 6 && currentHour < 12) {
+    img.src = "morning.png";
+    msg.innerHTML += "<p>Good Morning!</p>";
     window.document.body.style.background = "#da955d";
-  } else if (horaAtual >= 12 && horaAtual < 18) {
-    img.src = "tarde.png";
-    msg.innerHTML += "<p>Boa Tarde!</p>";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    img.src = "afternoon.png";
+    msg.innerHTML += "<p>Good Afternoon!</p>";
     window.document.body.style.background = "#CA6F0D";
-  } else if (horaAtual >= 0 && horaAtual < 6) {
-    img.src = "noite.png";
-    msg.innerHTML += "<p>Boa Madrugada!</p>";
+  } else if (currentHour >= 0 && currentHour < 6) {
+    img.src = "night.png";
+    msg.innerHTML += "<p>Good Dawn!</p>";
     window.document.body.style.background = "#513859";
   } else {
-    //Boa Noite!
-    img.src = "noite.png";
-    msg.innerHTML += "<p>Boa Noite!</p>";
+    // Good Night!
+    img.src = "night.png";
+    msg.innerHTML += "<p>Good Night!</p>";
     window.document.body.style.background = "#513859";
   }
 }
